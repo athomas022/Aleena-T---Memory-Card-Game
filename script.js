a3619a39d38213ef5fc5acb78ddcfda4d9df2575
Aleena Thomas <aleenatom92@gmail.com>
2024-01-20 17:25:27 -0600

    JS for landing page done

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-//WELCOME PROMPT
-let gamerName = document.querySelector("input");
+// PART 1: LANDING PAGE NAVIGATION
+//TASK 1.1: CREATING A GAMER OBJECT
+ const gamer = {
+        gamerName: "", // Setting the name to no value and to be populated by gamer
+        winStatus: false, // default is that they have not won at the begining of the game
+    };
+
+//TASK 1.2: CREATING WELCOME PROMPT IN THE WBEPAGE
+let gamerInput = document.querySelector("#name-input"); // Selected the input element
+// function below will execute when the button on the html is clicked
+function grabName(){
+gamer.gamerName = gamerInput.value
+}
+grabName();
+function openGame(){
+window.location.href = "http://127.0.0.1:5500/index_2.html" 
+}
+
+
+console.log(gamer);
+console.log(openGame);
+
+// PART 2: PLAY THE GAME
+let memoryCards = [];
 
 
 
 
 
-
-// SELECT THE CARDS
-
-
-
-
-
-
-//
+//PART 3: RESTART GAME OR EXIT
